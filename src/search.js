ec9eb93b00cc2e7dc1b10854e09204f273ece9d9
michael belete <it.michael.belete@gmail.com>
2022-03-19 04:28:13 +0300

    finished the search

--- src/search.js
+++ src/search.js
@@ -1,8 +1,9 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { useParams } from "react-router-dom";
-/* eslint-disable react-hooks/rules-of-hooks */
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { searchRecipes } from "./utils/queries.js";
+import { useInfiniteQuery } from "react-query";
+import { useInView } from "react-intersection-observer";
 import RecipeCard from "./components/RecipeCard";
 import { AiOutlineSearch } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
@@ -12,59 +13,108 @@ export default function search() {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   let { q } = useParams();
 
+  const { ref, inView } = useInView();
   const navigate = useNavigate();
   const [searchQuery, setSearchQuery] = useState("");
 
+  //   const { status, data, error } = useQuery(['recipes', q], searchRecipes)
+  const {
+    status,
+    data,
+    error,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery(["recipes", q], searchRecipes, {
+    getNextPageParam: (lastPage) => lastPage.page ?? undefined,
+  });
+
+  const resultCount = data?.pages[0].posts.length
   const submitData = async (e) => {
     e.preventDefault();
-    navigate('/'+searchQuery, {replace: true});
+    navigate("/" + searchQuery, { replace: true });
   };
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [fetchNextPage, inView]);
 
   return (
     <>
-    <Headers header={false}/>
-    <section className="bg-white py-8">
-      <div className="container mx-auto flex items-center flex-wrap pt-4 pb-12">
-        <nav id="store" className="w-full z-30 top-0 px-6 py-1">
-          <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-3">
-            <a
-              className="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl "
-              href="/"
-              id="recipes"
-            >
-              20 Result(s) Found
-            </a>
-
-            <form
-              onSubmit={submitData}
-              className="flex items-center"
-              id="store-nav-content"
-            >
-              <input
-                type="text"
-                className="border rounded py-1 px-3"
-                placeholder="search recipe..."
-                value={searchQuery}
-                required
-                onChange={(e) => setSearchQuery(e.target.value)}
-              />
-
-              <button
-                disabled={searchQuery == null}
-                type="submit"
-                className="pl-3 inline-block no-underline hover:text-black"
+      <Headers header={false} />
+      <section className="bg-white py-8">
+        <div className="container mx-auto flex items-center flex-wrap pt-4 pb-12">
+          <nav id="store" className="w-full z-30 top-0 px-6 py-1">
+            <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-3">
+              <a
+                className="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl "
                 href="/"
+                id="recipes"
               >
-                <AiOutlineSearch size={25} />
-              </button>
-            </form>
-          </div>
-        </nav>
-       
+                {resultCount} Result(s) Found
+              </a>
 
-      </div>
-    </section>
-    <Footers />
+              <form
+                onSubmit={submitData}
+                className="flex items-center"
+                id="store-nav-content"
+              >
+                <input
+                  type="text"
+                  className="border rounded py-1 px-3"
+                  placeholder="search recipe..."
+                  value={searchQuery}
+                  required
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                />
+
+                <button
+                  disabled={searchQuery == null}
+                  type="submit"
+                  className="pl-3 inline-block no-underline hover:text-black"
+                  href="/"
+                >
+                  <AiOutlineSearch size={25} />
+                </button>
+              </form>
+            </div>
+          </nav>
+          {status === "loading" ? (
+            <p>Loading...</p>
+          ) : status === "error" ? (
+            <span>Error: {error.message}</span>
+          ) : (
+            <>
+              {data.pages.map((page) => (
+                <>
+                  {page.posts.map((recipe) => (
+                    <RecipeCard post={recipe} />
+                  ))}
+                </>
+              ))}
+            </>
+          )}
+          <div className="w-full mt-10 lg:px-96 lg:mx-36 px-8">
+            <button
+              ref={ref}
+              onClick={() => fetchNextPage()}
+              disabled={!hasNextPage || isFetchingNextPage}
+              className="bg-red-900 hover:bg-red-700 py-2 px-4 rounded-xl text-white"
+            >
+              <center>
+                {isFetchingNextPage
+                  ? "Loading more..."
+                  : hasNextPage
+                  ? "Load Newer"
+                  : "Nothing more to load"}
+              </center>
+            </button>
+          </div>
+        </div>
+      </section>
+      <Footers />
     </>
   );
 }
--- src/utils/queries.js
+++ src/utils/queries.js
@@ -14,20 +14,17 @@ export async function fetchRecipes({ pageParam = null }) {
 }
 
 //search recipes 
-export async function searchRecipes({ queryKey }) {
+export async function searchRecipes({queryKey, pageParam = null}) {
   // eslint-disable-next-line no-unused-vars
-  const [_, q] = queryKey
 
-  const res = await axios.get(apiEndpoint + "recipes?search=" + q);
+  const q = queryKey[1];
+  let res;
+
+  if(pageParam == null) {
+    res = await axios.get(apiEndpoint + "recipes?search=" + q);
+  } else{
+    res = await axios.get(apiEndpoint + "recipes?search="+ q + "&&page=" + pageParam);
+  }
 
   return res.data;
 }
-// export async function fetchRecipes({ pageParam = null }) {
-//   let res;
-//   if (pageParam == null) {
-//     res = await axios.get(apiEndpoint + "recipes");
-//   } else {
-//     res = await axios.get(apiEndpoint + "recipes?page=" + pageParam);
-//   }
-//   return res.data;
-// }
