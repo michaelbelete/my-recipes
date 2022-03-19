46d4222431823f59a08678605e00b37a0efbbf90
michael belete <it.michael.belete@gmail.com>
2022-03-19 05:03:41 +0300

    done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,4 @@
+import { AiFillTwitterCircle } from "react-icons/ai";
 export default function footers() {
     return(
         <footer className="container mx-auto bg-white py-8 border-t border-gray-400">
@@ -7,7 +8,7 @@ export default function footers() {
                     <div className="px-3 md:px-0">
                         <h3 className="font-bold text-gray-900">About</h3>
                         <p className="py-4">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel mi ut felis tempus commodo nec id erat. Suspendisse consectetur dapibus velit ut lacinia.
+                        The Repibox recipe extension for chrome helps you instantly jump to the recipe. Repibox will detect if a page contains a recipe and will automatically display the ingredients and instructions at the top of the page so that you can cook distraction free.
                         </p>
                     </div>
                 </div>
@@ -16,13 +17,17 @@ export default function footers() {
                         <h3 className="font-bold text-gray-900">Social</h3>
                         <ul className="list-reset items-center pt-3">
                             <li>
-                                <a className="inline-block no-underline hover:text-black hover:underline py-1" href="/">Add social links</a>
+                                <a className="inline-block no-underline hover:text-black hover:underline py-1" href="https://twitter.com/repibox?_ga=2.55956717.797439766.1647617721-564109100.1647442327"><AiFillTwitterCircle size={40} className="text-blue-500"/></a>
                             </li>
                         </ul>
                     </div>
                 </div>
             </div>
         </div>
+
+        <center className="w-full text-lg font-bold">
+                Proudly Developed by <a href="https://www.linkedin.com/in/betselot-getnet-2423561aa/" className="text-blue-500">betslot getnet</a>
+            </center>
     </footer>
     ) 
 }
